e6ee8c7f75332ee47aa918a7d45e3801cb7f92fc
mjallen729 <lumpychum@gmail.com>
2024-10-14 17:21:39 -0400

    implemented driver

--- CS590 Algorithms/Module4 Prog Project/main.py
+++ CS590 Algorithms/Module4 Prog Project/main.py
@@ -1,16 +1,21 @@
-# TODO driver to process input data / logic and interact with microservices
+# Driver to process input data / logic and interact with microservices
+import services.database_service as dbserv
 
 inputs = open('./test_cases.txt', 'r')
 
-def process_input() -> dict[str, list | int]:
+def process_input() -> dict[str, list | int] | None:
   '''
-  Process test case input and return a dict containing
+  Process a test case input and return a dict containing
     k_highest: num of upgrade slots avail, 
     passengers: list of passenger data,
     cancels: list of passengers who canceled
   '''
   proc = dict()
   start = inputs.readline().split()
+
+  if not start:
+    return None
+
   num_flyers = int(start[0])
   proc['k_highest'] = int(start[1])
   num_cancels = int(start[2])
@@ -32,11 +37,32 @@ def process_input() -> dict[str, list | int]:
   inputs.readline()  # process empty line
   cancels = list()
   for i in range(num_cancels):
-    cancels.append(int(inputs.readline().split()[0]))
+    line = inputs.readline()
+    cancels.append(int(line.split()[0]))
 
   proc['cancels'] = cancels
   inputs.readline()  # process empty line after test case
 
   return proc
 
-print(process_input())
+while True:
+  processed = process_input()
+
+  if not processed:
+    break
+
+  database = dbserv.DatabaseService()
+
+  # add the passengers to the database
+  for passenger in processed['passengers']:
+    database.add_passenger(passenger)
+
+  # cancel any who wish to cancel
+  for flyer_id in processed['cancels']:
+    database.cancel_passenger(flyer_id)
+
+  # return list of passengers eligible for k upgrade slots
+  eligible = database.get_top_k(processed['k_highest'])
+
+  for p in eligible:
+    print(p)
