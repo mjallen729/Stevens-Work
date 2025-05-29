a8cdb64c76dc6efe156f522f2826b34cf34eb03f
Matt <matthew@lialtaholdings.com>
2025-05-29 00:01:32 -0400

    lab1 pt 2

--- CS546 Web Programming/Lab1_stub/lab1.mjs
+++ CS546 Web Programming/Lab1_stub/lab1.mjs
@@ -6,6 +6,11 @@ export const questionOne = (arr) => {
   let newArr = [];
 
   for (const elem of arr) {
+    if (elem == 0) {
+      newArr.push(1);
+      continue;
+    }
+
     let res = 1;
 
     for (let i = elem; i > 0; i--) {
@@ -26,17 +31,23 @@ export const questionTwo = (arr) => {
   // return an empty object.You do not have to worry about dealing with different data types passed
   // in.You can assume only arrays and numbers as elements will be passed in to your function (we
   // get to type checking and error handling in lecture 2)
-  if (arr.length == 0) {
-    return {};
-  }
+  if (arr.length == 0) return {};
 
   // To calc if prime, the best way to do it is trial division for numbers < 1M
   // Do trial division until reaching the square root ceil
   const ret = {};
 
   for (const num of arr) {
-    ret[num] = true; // start by assuming it is prime
     let sq = Math.ceil(Math.sqrt(num));
+    ret[num] = true; // start by assuming it is prime
+
+    if (num == 1 || num == 2) {
+      continue;
+    }
+    if (num == 0) {
+      ret[num] = false;
+      continue;
+    }
 
     for (let i = 2; i <= sq; i++) {
       if (num % i == 0) {
@@ -56,6 +67,29 @@ export const questionThree = (str) => {
   // If an empty string is passed in, just return 0 for each key. You do not have to worry about
   // dealing with different data types passed in. You can assume only strings will be passed in to
   // your function (we get to type checking and error handling in lecture 2).
+  const counts = {
+    uppercase: 0,
+    lowercase: 0,
+    number: 0,
+    whitespace: 0,
+    other: 0,
+  };
+
+  for (const char of str) {
+    if (/[A-Z]/.test(char)) {
+      counts.uppercase++;
+    } else if (/[a-z]/.test(char)) {
+      counts.lowercase++;
+    } else if (/[0-9]/.test(char)) {
+      counts.number++;
+    } else if (/\s/.test(char)) {
+      counts.whitespace++;
+    } else {
+      counts.other++;
+    }
+  }
+
+  return counts;
 };
 
 export const questionFour = (arr) => {
@@ -63,11 +97,47 @@ export const questionFour = (arr) => {
   // lexicographical order.The output array should first list all numbers followed by all strings.
   // Note: You do not have to worry about special characters in the strings supplied.
   // We will only test with numbers and strings that have letters(or numbers) in the string.
+  const compare = (a, b) => {
+    const isNumA = typeof a === "number";
+    const isNumB = typeof b === "number";
+
+    if (isNumA && isNumB) {
+      return a - b;
+    }
+    if (isNumA && !isNumB) {
+      return -1;
+    }
+    if (!isNumA && isNumB) {
+      return 1;
+    }
+    // both strings
+    return a.localeCompare(b);
+  };
+
+  const quickSort = (arr) => {
+    if (arr.length < 2) return arr;
+    const pivot = arr[Math.floor(arr.length / 2)];
+    const left = [],
+      middle = [],
+      right = [];
+
+    for (const elem of arr) {
+      const cmp = compare(elem, pivot);
+      if (cmp < 0) left.push(elem);
+      else if (cmp > 0) right.push(elem);
+      else middle.push(elem);
+    }
+
+    return [...quickSort(left), ...middle, ...quickSort(right)];
+  };
+
+  // work on a copy so we don’t mutate the input
+  return quickSort([...arr]);
 };
 
 //DO NOT FORGET TO UPDATE THE INFORMATION BELOW OR IT WILL BE -2 POINTS PER FIELD THAT IS MISSING.
 export const studentInfo = {
-  firstName: "YOUR FIRST NAME",
-  lastName: "YOUR LAST NAME",
-  studentId: "YOUR STUDENT ID",
+  firstName: "Matthew",
+  lastName: "Allen",
+  studentId: "20035604",
 };
--- CS546 Web Programming/Lab1_stub/lab1.test.mjs
+++ CS546 Web Programming/Lab1_stub/lab1.test.mjs
@@ -1,9 +1,29 @@
 import * as lab1 from "./lab1.mjs";
 
 // Question 1: Factorial
-console.log(lab1.questionOne([5, 0, 3]));
+console.log(lab1.questionOne([5, 0, 3])); // [120, 1, 6]
+console.log(lab1.questionOne([4, 2, 7])); // [24, 2, 5040]
+console.log(lab1.questionOne([3, 8, 9])); // [6, 40230, 362880]
+console.log(lab1.questionOne([4])); // [24]
+console.log(lab1.questionOne([1, 0])); // [1, 1]
 
 // Question 2: Is Prime
-console.log(lab1.questionTwo([5, 3, 10]));
+console.log(lab1.questionTwo([5, 3, 10])); // {5:true, 3: true, 10: false}
+console.log(lab1.questionTwo([2])); // {2: true}
+console.log(lab1.questionTwo([5, 10, 9])); // {5: true, 10: false, 9: false}
+console.log(lab1.questionTwo([2, 7, 9, 1013])); // {2: true, 7: true, 9: false, 1013: true}
+console.log(lab1.questionTwo([])); // {}
 
-// Question 3:
+// // Question 3: Character Type Counts
+console.log(lab1.questionThree("Hello123_ 90 ")); // { uppercase: 1, lowercase: 4, number: 5, whitespace: 2, other: 1 }
+console.log(lab1.questionThree("Hello World  123!")); // {uppercase: 2, lowercase: 8, numbers: 3, spaces: 3, otherCharacters: 1}
+console.log(lab1.questionThree("1824  XYabef!")); // {uppercase: 2, lowercase: 4, numbers: 4, spaces: 2, otherCharacters: 1}
+console.log(lab1.questionThree("")); // {uppercase: 0, lowercase: 0, numbers: 0, spaces: 0, otherCharacters: 0}
+console.log(lab1.questionThree("     ")); // {uppercase: 0, lowercase: 0, numbers: 0, spaces: 5, otherCharacters: 0}
+
+// // Question 4: Sorting
+console.log(lab1.questionFour([3, "guitar", 1, "bass", -10])); // [-10, 1, 3, "bass", "guitar"]
+console.log(lab1.questionFour(["a", 2, 1, "f", "car", "czd"])); // [1, 2, 'a', 'car', 'czd', 'f']
+console.log(lab1.questionFour([0, "Patrick", 100, "Hill", -50])); // [-50, 0, 100, "Hill", "Patrick"]
+console.log(lab1.questionFour([123, "World", 500, "Hello"])); // [123, 500, "Hello", "World"]
+console.log(lab1.questionFour(["b", "z", "r", 1, "2", "1"])); // [ 1, '1', '2', 'b', 'r', 'z' ]
