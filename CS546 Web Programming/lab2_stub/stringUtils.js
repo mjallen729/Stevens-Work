bb39772c195c55249af166e0cde5fa5aecf158b2
mjallen729 <lumpychum@gmail.com>
2025-06-06 21:30:42 -0400

    lab2 stub

---
+++ CS546 Web Programming/lab2_stub/stringUtils.js
@@ -0,0 +1,10 @@
+/* Todo: Implment the functions below and then export them
+      using the module.exports syntax. 
+      DO NOT CHANGE THE FUNCTION NAMES
+*/
+
+let camelCase = (str) => {};
+
+let replaceCharsAtIndexes = (str, idxArr) => {};
+
+let compressString = (str) => {};
